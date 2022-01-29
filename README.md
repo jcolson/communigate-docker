0a9c8af3fdb1efd220c10fae50925a9ec63cf4e4
Jay Colson <jay@karma.net>
2022-01-29 06:14:23 +0000

    properly shutting down with docker now

--- README.md
+++ README.md
@@ -52,3 +52,20 @@ karmanet/communigate:6.2.14-amd64
 ```
 
 docker inspect -f '{{.State.ExitCode}}' communigate
+
+## ports
+
+```shell
+    –	sgr-0dbaf02a76e704843	IPv4	IMAPS	TCP	993	0.0.0.0/0	–
+	–	sgr-03753b03d34887893	IPv4	SMTPS	TCP	465	0.0.0.0/0	–
+	–	sgr-045c135535e8087dd	IPv6	SMTPS	TCP	465	::/0	–
+	–	sgr-0098a683c8bde7566	IPv4	SMTP	TCP	25	0.0.0.0/0	–
+	–	sgr-026896d9a6a346de0	IPv6	SMTP	TCP	25	::/0	–
+	–	sgr-08ba6d99aad620f60	IPv6	POP3	TCP	110	::/0	–
+	–	sgr-0ec74f0e76f01bbb4	IPv4	POP3S	TCP	995	0.0.0.0/0	–
+	–	sgr-08c709e6f09f9910a	IPv4	IMAP	TCP	143	0.0.0.0/0	–
+	–	sgr-0cbc99ebf5af5d798	IPv6	IMAP	TCP	143	::/0	–
+	–	sgr-0ed014b2f4134e44d	IPv6	POP3S	TCP	995	::/0	–
+	–	sgr-08011065a08d49f91	IPv4	POP3	TCP	110	0.0.0.0/0	–
+	–	sgr-0dc87564f393d1682	IPv6	IMAPS	TCP	993	::/0	–
+```
--- bin/communigate.sh
+++ bin/communigate.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env sh
+APPLICATION="/opt/CommuniGate"
+BASEFOLDER="/var/CommuniGate"
+SUPPLPARAMS=
+[ -f ${APPLICATION}/CGServer ] || exit 0
+ulimit -c 2097151
+umask 0
 rm -f /var/CommuniGate/ProcessID
-/etc/init.d/CommuniGate start
-sleep 5
-trap "echo Caught Trap && /etc/init.d/CommuniGate stop" 1 2 3 6 15
-tail --pid=`cat /var/CommuniGate/ProcessID` -f /dev/null
+if [ -d ${BASEFOLDER} ] ; then
+    echo "${BASEFOLDER} already exists..."
+else
+    echo "Creating the CommuniGate Base Folder..."
+    mkdir ${BASEFOLDER}
+    chgrp mail ${BASEFOLDER}
+    chmod 2770 ${BASEFOLDER}
+fi
+echo "Starting CommuniGate Pro"
+exec ${APPLICATION}/CGServer \
+    --Base ${BASEFOLDER} \
+#   --Daemon ${SUPPLPARAMS} \
+#	--ClusterBackend
+#	--ClusterFrontend
+
+#    touch /var/lock/subsys/CommuniGate
